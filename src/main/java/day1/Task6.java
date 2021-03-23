ef5d83ac6a4b3e4c446a01098396abe65d158623
Orken <orkenfreeman@gmail.com>
2021-03-23 21:25:09 +0600

    Added day1 tasks

--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -1,7 +1,13 @@
 package day1;
 
+import java.util.Scanner;
+
 public class Task6 {
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
+        int k = in.nextInt();
+        for (int i = 1; i <= 9; i++) {
+            System.out.printf("%d x %d = %d%n", i,k,i*k);
+        }
     }
 }
